c303f82b30ffab931414d9481baefa078aff7e7e
Taryn East <git@taryneast.org>
2019-04-08 15:11:39 +1000

    tolower the letter

--- ex10_moreargs.c
+++ ex10_moreargs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
@@ -14,34 +15,28 @@ int main(int argc, char *argv[]) {
 
         for (int i = 0; (letter = argv[n][i]) != '\0'; i++) {
 
-            switch (letter) {
+            switch (tolower(letter)) {
                 case 'a':
-                case 'A':
                     printf("%d: 'A'\n", i);
                     break;
 
                 case 'e':
-                case 'E':
                     printf("%d: 'E'\n", i);
                     break;
 
                 case 'i':
-                case 'I':
                     printf("%d: 'I'\n", i);
                     break;
 
                 case 'o':
-                case 'O':
                     printf("%d: 'O'\n", i);
                     break;
 
                 case 'u':
-                case 'U':
                     printf("%d: 'U'\n", i);
                     break;
 
                 case 'y':
-                case 'Y':
                     if (i > 2) {
                         // it's only sometimes Y
                         printf("%d: 'Y'\n", i);
