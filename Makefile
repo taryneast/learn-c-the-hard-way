a4d6b06d8aaa6dc7f6b01a9506abf6edd99874b6
Taryn East <taryn@sendle.com>
2019-05-10 11:57:10 +1000

    exercise 16: Structs and pointers
    with better gender balance...

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS=-Wall -g
 
-all: clean ex1 ex2 ex3 ex7 ex8 ex9 ex10 ex11 ex12 ex13 ex14 ex15
+all: clean ex1 ex2 ex3 ex7 ex8 ex9 ex10 ex11 ex12 ex13 ex14 ex15 ex16
 
 clean:
-	rm -f ex1 ex2 ex3 ex7 ex8 ex9 ex10 ex11 ex12 ex13 ex14 ex15
+	rm -f ex1 ex2 ex3 ex7 ex8 ex9 ex10 ex11 ex12 ex13 ex14 ex15 ex16
 	rm -Rf ./*.dSYM
---
+++ ex16.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+    char *name;
+    int age;
+    int height;
+    int weight;
+};
+
+struct Person *Person_create(char *name, int age, int height, int weight) {
+    struct Person *who = malloc(sizeof(struct Person));
+    assert(who !=NULL);
+
+    who->name = strdup(name);
+    who->age = age;
+    who->height = height;
+    who->weight = weight;
+
+    return who;
+}
+
+void Person_destroy(struct Person *who) {
+    assert(who !=NULL);
+
+    free(who->name);
+    free(who);
+}
+
+void Person_print(struct Person *who) {
+    printf("Name: %s\n", who->name);
+    printf("Age: %d\n", who->age);
+    printf("Height: %d\n", who->height);
+    printf("Weight: %d\n", who->weight);
+}
+
+int main(int argc, char *argv[]) {
+    // make two people structures
+    struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
+    struct Person *jane = Person_create("Jane Black", 43, 54, 170);
+
+    // print them out and where they are in memory
+    printf("Joe is at memory location %p:\n", joe);
+    Person_print(joe);
+    printf("Jane is at memory location %p:\n", jane);
+    Person_print(jane);
+
+    // age everyone by 20 years and print them out again
+    joe->age += 20;
+    joe->height -= 2;
+    joe->weight += 40;
+    Person_print(joe);
+
+    jane->age += 20;
+    jane->height -= 3;
+    jane->weight -= 40;
+    Person_print(jane);
+
+    // destroy them both
+    Person_destroy(joe);
+    Person_destroy(jane);
+
+    return 0;
+}
